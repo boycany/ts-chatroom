25f9686ab43a7d4a4edc64c387ad1b67db069e80
boycany <yurayurateiko@gmail.com>
2022-11-24 17:41:30 +0800

    socket connection ok

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import devServer from "./server/dev";
 import prodServer from "./server/prod";
 import express from "express";
+import { Server } from 'socket.io'
+import http from 'http'
 
 import { name } from "@/utils";
 
 const port = 3000;
 const app = express();
+
+const server = http.createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket)=>{
+  socket.emit('join', "welcome")  //假設這裡將頻道名取為 join，對應到前端會有相同名字的頻道
+})
 
 // 執行npm run dev本地開發 or 執行npm run start部署後啟動線上伺服器
 if (process.env.NODE_ENV === "development") {
@@ -16,6 +25,6 @@ if (process.env.NODE_ENV === "development") {
 
 console.log("server side", name);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`The application is running on port ${port}.`);
 });
